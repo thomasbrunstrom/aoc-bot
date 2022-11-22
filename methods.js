997dc15f0d6f32610a429e1cc720a52b7a504e29
Englund <philip.englund@1337.tech>
2022-11-22 13:44:15 +0100

    change topic template

--- methods.js
+++ methods.js
@@ -14,9 +14,10 @@ const headers = {
 };
 
 const privateLeaderboardCode = process.env.privateLeaderboardCode;
+const sponsorJoinCode = process.env.sponsorJoinCode;
 let cache = { time: 0, data: null, stars: 0 };
 let channelTopic =
-  `:snowflake: AoC 2022: https://adventofcode.com :snowflake: 13|37 leaderboard: https://1337co.de/15 | Join our private leaderboard with code: ${privateLeaderboardCode} :snowflake: more info coming!`;
+  `:star:=0 | :snowflake: AoC 2022: https://adventofcode.com :snowflake: 13|37 leaderboard: https://1337co.de/15 | Join our private leaderboard with code: ${privateLeaderboardCode} :snowflake: Sponsor join code ${sponsorJoinCode} (internal use only)!:shushing_face:`;
 
 const buildCache = async () => {
   if (!cache.data || cache?.time < Date.now()) {
